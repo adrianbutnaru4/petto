f37abb55c53ebf70fc7f0aa423e3f0fe3d0a3e50
adrianbutnaru4 <adrian.butnaru4@gmail.com>
2020-08-09 00:50:12 +0300

    Removed unused code

--- petto-backend/auth-service/src/main/java/com/petto/auth/security/CustomUserDetailsService.java
+++ petto-backend/auth-service/src/main/java/com/petto/auth/security/CustomUserDetailsService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,15 +15,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private PettoUserClient pettoUserClient;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
         UserDto user = pettoUserClient.findByEmail(email);
-        String encoded = passwordEncoder.encode(user.getPassword());
 
         return UserPrincipal.create(user);
     }
