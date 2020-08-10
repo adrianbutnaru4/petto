23efc5dd9f1188e3b066d13d946f0789bd202312
adrianbutnaru4 <adrian.butnaru4@gmail.com>
2020-08-10 22:00:32 +0300

    Applied the google formatter to auth-service

--- petto-backend/auth-service/src/main/java/com/petto/auth/config/AppProperties.java
+++ petto-backend/auth-service/src/main/java/com/petto/auth/config/AppProperties.java
@@ -8,49 +8,49 @@ import java.util.List;
 
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
-    private final Auth auth = new Auth();
-    private final OAuth2 oauth2 = new OAuth2();
+  private final Auth auth = new Auth();
+  private final OAuth2 oauth2 = new OAuth2();
 
-    @Data
-    public static class Auth {
-        private String tokenSecret;
-        private long tokenExpirationMs;
+  @Data
+  public static class Auth {
+    private String tokenSecret;
+    private long tokenExpirationMs;
 
-        public String getTokenSecret() {
-            return tokenSecret;
-        }
-
-        public void setTokenSecret(String tokenSecret) {
-            this.tokenSecret = tokenSecret;
-        }
-
-        public long getTokenExpirationMs() {
-            return tokenExpirationMs;
-        }
-
-        public void setTokenExpirationMs(long tokenExpirationMs) {
-            this.tokenExpirationMs = tokenExpirationMs;
-        }
+    public String getTokenSecret() {
+      return tokenSecret;
     }
 
-    public static final class OAuth2 {
-        private List<String> authorizedRedirectUris = new ArrayList<>();
-
-        public List<String> getAuthorizedRedirectUris() {
-            return authorizedRedirectUris;
-        }
-
-        public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
-            this.authorizedRedirectUris = authorizedRedirectUris;
-            return this;
-        }
+    public void setTokenSecret(String tokenSecret) {
+      this.tokenSecret = tokenSecret;
     }
 
-    public Auth getAuth() {
-        return auth;
+    public long getTokenExpirationMs() {
+      return tokenExpirationMs;
     }
 
-    public OAuth2 getOauth2() {
-        return oauth2;
+    public void setTokenExpirationMs(long tokenExpirationMs) {
+      this.tokenExpirationMs = tokenExpirationMs;
     }
+  }
+
+  public static final class OAuth2 {
+    private List<String> authorizedRedirectUris = new ArrayList<>();
+
+    public List<String> getAuthorizedRedirectUris() {
+      return authorizedRedirectUris;
+    }
+
+    public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
+      this.authorizedRedirectUris = authorizedRedirectUris;
+      return this;
+    }
+  }
+
+  public Auth getAuth() {
+    return auth;
+  }
+
+  public OAuth2 getOauth2() {
+    return oauth2;
+  }
 }
--- petto-backend/auth-service/src/main/java/com/petto/auth/security/JwtUsernameAndPasswordAuthenticationFilter.java
+++ petto-backend/auth-service/src/main/java/com/petto/auth/security/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -21,51 +21,62 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.stream.Collectors;
 
-public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+public class JwtUsernameAndPasswordAuthenticationFilter
+    extends UsernamePasswordAuthenticationFilter {
 
-    private final AuthenticationManager authManager;
+  private final AuthenticationManager authManager;
 
-    private final JwtConfig jwtConfig;
+  private final JwtConfig jwtConfig;
 
-    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authManager, JwtConfig jwtConfig) {
-        this.authManager = authManager;
-        this.jwtConfig = jwtConfig;
+  public JwtUsernameAndPasswordAuthenticationFilter(
+      AuthenticationManager authManager, JwtConfig jwtConfig) {
+    this.authManager = authManager;
+    this.jwtConfig = jwtConfig;
 
-        this.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher(jwtConfig.getUri(), "POST"));
+    this.setRequiresAuthenticationRequestMatcher(
+        new AntPathRequestMatcher(jwtConfig.getUri(), "POST"));
+  }
+
+  @Override
+  public Authentication attemptAuthentication(
+      HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+    try {
+
+      LoginRequest creds =
+          new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
+
+      UsernamePasswordAuthenticationToken authToken =
+          new UsernamePasswordAuthenticationToken(creds.getUsername(), creds.getPassword());
+
+      return authManager.authenticate(authToken);
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-            throws AuthenticationException {
+  @Override
+  protected void successfulAuthentication(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain chain,
+      Authentication auth)
+      throws IOException, ServletException {
+    long now = System.currentTimeMillis();
+    String token =
+        Jwts.builder()
+            .setSubject(auth.getName())
+            .claim(
+                "authorities",
+                auth.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList()))
+            .setIssuedAt(new Date(now))
+            .setExpiration(new Date(now + jwtConfig.getExpiration() * 1000)) // in milliseconds
+            .signWith(SignatureAlgorithm.HS256, jwtConfig.getSecret().getBytes())
+            .compact();
 
-        try {
-
-            LoginRequest creds = new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
-
-            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                    creds.getUsername(), creds.getPassword());
-
-            return authManager.authenticate(authToken);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-                                            Authentication auth) throws IOException, ServletException {
-        long now = System.currentTimeMillis();
-        String token = Jwts.builder()
-                .setSubject(auth.getName())
-                .claim("authorities", auth.getAuthorities().stream()
-                        .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-                .setIssuedAt(new Date(now))
-                .setExpiration(new Date(now + jwtConfig.getExpiration() * 1000))  // in milliseconds
-                .signWith(SignatureAlgorithm.HS256, jwtConfig.getSecret().getBytes())
-                .compact();
-
-        response.addHeader(jwtConfig.getHeader(), jwtConfig.getPrefix() + token);
-    }
-
+    response.addHeader(jwtConfig.getHeader(), jwtConfig.getPrefix() + token);
+  }
 }
